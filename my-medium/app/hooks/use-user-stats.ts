a79a7dc15072e12d1c3a763d5c60dc918d90bb95
muziga judith <umuzigajudit@gmail.com>
2025-11-18 10:10:59 +0200

    added users stats

---
+++ my-medium/app/hooks/use-user-stats.ts
@@ -0,0 +1,24 @@
+'use client'
+
+import { usePosts } from '../lib/post-contex'
+import { useSocial } from '../lib/social-context'
+import { useMemo } from 'react'
+
+export function useUserStats(userId: string) {
+  const { posts } = usePosts()
+  const { getLikeCount, getFollowerCount, getFollowingCount } = useSocial()
+
+  return useMemo(() => {
+    const userPosts = posts.filter(p => p.authorId === userId)
+    
+    const stats = {
+      postCount: userPosts.length,
+      totalViews: userPosts.reduce((sum, p) => sum + (p.views || 0), 0),
+      totalLikes: userPosts.reduce((sum, p) => sum + getLikeCount(p.id), 0),
+      followers: getFollowerCount(userId),
+      following: getFollowingCount(userId)
+    }
+
+    return stats
+  }, [userId, posts, getLikeCount, getFollowerCount, getFollowingCount])
+}
